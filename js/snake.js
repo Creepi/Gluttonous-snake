d9f6be626fa81883e2157438070b71806e5b5bf6
Creepiz <2865729331@qq.com>
2018-03-06 13:45:42 +0800

    [snake move]

--- js/snake.js
+++ js/snake.js
@@ -6,6 +6,7 @@ window.onload = function () {
         this.snakeArr = [] //蛇本体
         this.keyButton = 39 //键盘操作
         this.speed = 10
+        this.timer = null //计时器
         this.direction = 'right'
         this.init()
     }
@@ -21,7 +22,7 @@ window.onload = function () {
             console.log(this.gridArr)
             this.createMap()
             this.createSnake()
-            document.onkeydown = this.keyDown
+            document.onkeydown = this.keyDown.bind(that)
         },
         createMap: function () {
             //初始化地图
@@ -41,9 +42,15 @@ window.onload = function () {
         },
         createSnake: function () {
             //初始化蛇
-            this.snakeArr = [],
-                this.snakeArr.push([1, 1])
+			this.snakeArr = [] ;
+			this.snakeArr.push([1,3]);
+			this.snakeArr.push([1,2]);
+			this.snakeArr.push([1,1]);
             this.gridArr[this.snakeArr[0][0]][this.snakeArr[0][1]].className = "snake_head";
+            this.gridArr[this.snakeArr[1][0]][this.snakeArr[1][1]].className = "snake_body";
+            this.gridArr[this.snakeArr[2][0]][this.snakeArr[2][1]].className = "snake_body";
+
+
         },
         keyDown: function (event) {
             let e = event || window.event;
@@ -51,13 +58,67 @@ window.onload = function () {
             if (keycode >= 37 && keycode <= 40) {
                 this.keyButton = keycode
             }
+            this.move()
             console.log(this.keyButton)
+        },
+        mount: function () {
+            let head_x = this.snakeArr[0][0]
+            let head_y = this.snakeArr[0][1]
+            switch (this.keyButton) {
+                case 37:
+                    if (this.pos_y != 1) {
+                        this.pos_y = -1;
+                        this.pos_x = 0
+                    } //防止控制蛇往相反反方向走
+                    break;
+                case 38:
+                    if (this.pos_x != 1) {
+                        this.pos_x = -1;
+                        this.pos_y = 0
+                    }
+                    break;
+                case 39:
+                    if (this.pos_y != -1) {
+                        this.pos_y = 1;
+                        this.pos_x = 0
+                    }
+                    break;
+                case 40:
+                    if (this.pos_x != -1) {
+                        this.pos_x = 1;
+                        this.pos_y = 0
+                    }
+            }
+            head_x += this.pos_x;
+            head_y += this.pos_y;
+            //去除蛇最后一个部分
+            this.gridArr[this.snakeArr[this.snakeArr.length - 1][0]][this.snakeArr[this.snakeArr.length - 1][1]].className = "";
+            for (let i = this.snakeArr.length - 1; i > 0; i--) {
+                this.snakeArr[i] = this.snakeArr[i - 1];
+            }
+            this.snakeArr[0] = [head_x, head_y];
+            console.log(this.snakeArr.length - 1)
+            this.drawSnake()
+            this.gridArr[head_x][head_y].className = "snake_head";
+
         },
         move: function () {
             //控制函数
+            let that = this;
+            if (that.timer) {
+                clearInterval(that.timer);
+            }
+            that.timer = setInterval(that.mount.bind(that), Math.floor(3000 / this.speed));
             console.log(this.keyButton)
-            
-
+        },
+        drawSnake: function () {
+            //绘制蛇身体
+            for (let i = 0; i < this.snakeArr.length; i++) {
+                let snake_temp1 = this.snakeArr[i][0],
+                    snake_temp2 = this.snakeArr[i][1];
+                this.gridArr[snake_temp1][snake_temp2].className = "snake_body";
+                this.gridArr[snake_temp1][snake_temp2].className = "snake_body";
+            }
         }
     }
     new Snake()
